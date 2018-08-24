a1c802ee1d207c1efea622c5eb0ee6e85692377c
Neil Brubacher <neil.brubacher@hotmail.com>
2018-08-24 01:06:32 -0400

    Adds feed scrolling and live-updating

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import InfiniteScroll from 'react-infinite-scroller';
+import _ from 'lodash';
 
 import Post from '../Post/PostComponent';
 import api from '../../ApiClient';
 
-const DEFAULT_POLL = 60 * 1000; // 1 minute
+const PAGE_SIZE = 15;
+const DEFAULT_POLL = 10 * 1000; // 1 minute
 
 
 export default class Feed extends Component {
 
-  state = {};
+  state = {
+    page: 1,
+    posts: []
+  };
 
   getUri = () => {
     return '/posts';
@@ -33,26 +39,34 @@ export default class Feed extends Component {
 
   async componentWillMount() {
 
+    await this.loadData();
+
+    // keep this in case we want to clear it
+    var pollIntervalId = setInterval(() => {
+      console.warn("reloading")
+      this.loadData({ reload: true });
+    }, DEFAULT_POLL);
+    
+    this.setState({ pollIntervalId });
+  }
+
+  loadData = async (options = {}) => {
+    var isReload = options.reload;
+
+    var page;
+    if (!isReload) page = this.state.page;
+    else page = 1;
+
     this.setState({
       loading: true
     });
-
-    await this.loadData();
-
-    this.setState({
-      loading: false
-    });
-
-    var pollInterval = setInterval(this.loadData, DEFAULT_POLL);
-
-    this.setState({ loading: false, pollInterval });
-  }
-
-  loadData = async () => {
-    await api.get(this.getUri())
+    await api.get(this.getUri(), { page })
       .then(response => {
+        if (isReload) return this.updateRecent(response);
         this.setState({
-          posts: response,
+          posts: this.state.posts.concat(response),
+          loadedLastPage: response.length < PAGE_SIZE,
+          loading: false
         });
       })
       .catch((err) => {
@@ -60,15 +74,64 @@ export default class Feed extends Component {
       });
   }
 
-  render() {
-    var { posts, loading } = this.state;
+  /**
+   * Will take PAGE_SIZE most recent posts and either update or add them to the feed.
+   */
+  updateRecent(freshData) {
+    const { posts } = this.state;
 
-    if (loading) {
-      return (<div>Loading...</div>)
+    // Triage new and pre-existing posts
+    var newPosts = [];
+    var oldPosts = [];
+    _.each(freshData, item => {
+      var existing = _.find(posts, post => post._id === item._id);
+      if (existing) oldPosts.push(item);
+      else newPosts.push(item);
+    });
+
+    var numNew = newPosts.length;
+    var updated = newPosts.concat(oldPosts);
+    var numUpdated = updated.length;
+
+    // with 15 fresh posts coming in with 3 new, we'd want to replace the first 12
+    var toReturn = posts.slice(numUpdated - numNew);
+
+    toReturn.unshift(...updated);
+
+    this.setState({
+      posts: toReturn,
+      loading: false
+    })
+
+  }
+
+  loadNext = () => {
+    if (this.state.loading) return;
+    this.setState({ page: this.state.page + 1, loading: true });
+    this.loadData();
+  }
+
+  render() {
+    var {
+      posts,
+      loadedLastPage,
+      loading
+    } = this.state;
+
+    if (!posts) {
+      return (<div>Loading ...</div>)
     }
 
+    var posts = posts.map((post, i) => <Post key={`post-${i}`} data={post} />)
+
     return (
-      posts.map(post => <Post data={post} />)
+      <InfiniteScroll
+        loadMore={this.loadNext.bind(this)}
+        loader={<div key={0}>Loading ...</div>}
+        hasMore={!loading && !loadedLastPage}
+      >
+        {posts}
+      </InfiniteScroll>
     )
   }
 }
