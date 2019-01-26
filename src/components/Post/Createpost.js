701845a8e1d4adf20332a4d374f05a3995919a4c
ERIK MOHR <edmohr@edu.uwaterloo.ca>
2019-01-26 17:20:18 -0500

    basic UI and function for local file upload. todo ui polish and send image to server

--- src/components/Post/Createpost.js
+++ src/components/Post/Createpost.js
@@ -3,16 +3,42 @@ import React from 'react';
 import './PostComponent.css';
 import Button from '../Shared/Button/Button';
 import './CreatePost.css';
+import CatIcon from '../../assets/catIcon.png';
+import ImageUploader from 'react-images-upload';
 
 export default class CreatePost extends React.Component {
-
-  state = {}
+  constructor(props) {
+    super(props);
+    this.state = { image: null, uploadInterface: false };
+    this.onDrop = this.onDrop.bind(this);
+  }
 
   updateContent = (event) => {
     this.setState({
       postContent: event.target.value,
       ref: event.target
     });
+  }
+
+ onDrop(picture) {
+    if(picture.length === 0) {
+      this.setState({
+        image: null,
+      });
+    }
+    else{
+      this.setState({
+          image: picture,
+      });
+    }
+  }
+
+  getImageIcon = () => {
+    return CatIcon;
+  }
+
+  showUploadInterface = () => {
+    this.setState({uploadInterface: !this.state.uploadInterface});
   }
 
   addPost = () => {
@@ -31,11 +57,32 @@ export default class CreatePost extends React.Component {
 
   render() {
     const { channel } = this.props;
+    const imageIcon = this.getImageIcon();
     return (
       <div className="card creationCard">
         {/* <div className="headerContent">
           {`Post to ${channel.name}`}
         </div> */}
+        <div className="postAddons">
+          {this.state.uploadInterface &&
+            <ImageUploader
+              withIcon={true}
+              buttonText='Choose images'
+              buttonStyles={{display: this.state.image ? 'none' : 'inline'}}
+              onChange={this.onDrop}
+              singleImage={true}
+              withPreview
+              imgExtension={['.jpg', '.gif', '.png']}
+              maxFileSize={5242880}
+            />
+          }
+          <img
+            className="addPhoto"
+            style={{maxHeight: '40px'}}
+            src={imageIcon}
+            onClick={this.showUploadInterface}
+          />
+        </div>
         <div className="content">
           <textarea
             className="creationField"
