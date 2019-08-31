c95bc9a172fb506bbaafa4a12e10946f3feefbdc
Ryan Martin <martr4929@hotmail.com>
2019-08-31 01:13:57 -0400

    Fix environment issue

--- src/config.js
+++ src/config.js
@@ -1,9 +1,8 @@
 let address = 'https://skybunk-auth-dev.herokuapp.com';
 
-if (process.env.AUTH_ADDRESS) {
-	address = process.env.AUTH_ADDRESS;
+if (process.env.REACT_APP_AUTH_ADDRESS) {
+	address = process.env.REACT_APP_AUTH_ADDRESS;
 }
-console.log(address)
 module.exports = {
   AUTH_ADDRESS: address,
 }
